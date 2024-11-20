bd5a4e80e2554bdab41a3b207b3a0d98e0a9a920
Seraphiel <76918503+Pierre2650@users.noreply.github.com>
2024-11-20 18:16:19 +0100

    Unitary test exemple

---
+++ Planning_Poker_Conception_Agile/Assets/Scripts/UnitTest_Sum.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class UnitTest_Sum
+{
+
+    public int add (int x, int y){
+        return x + y;
+    }
+
+
+    public int subtract (int x, int y) { 
+        return x - y; 
+    }
+   
+}
