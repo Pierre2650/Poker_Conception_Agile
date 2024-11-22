334dbe509f0da6447840f639faf45e7d79308759
Seraphiel <76918503+Pierre2650@users.noreply.github.com>
2024-11-22 18:46:58 +0100

    Test Doxygen

--- Planning_Poker_Conception_Agile/Assets/Scripts/UnitTest_Sum.cs
+++ Planning_Poker_Conception_Agile/Assets/Scripts/UnitTest_Sum.cs
@@ -1,15 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 
+/*
+@file
+@brief Test Description: Script pour tester l'integration Continue.
+@author Larmat jean
+ */
+
+
 public class UnitTest_Sum
 {
+    /*
+    @class UnitTest_Sum_Class
+    @brief Une Classe pour tester que ca fonctionne
 
+     */
     public int add (int x, int y){
+        /*
+         * @brief Methode pour simuler une addition
+         * 
+         * @param x: Premiere Chiffre
+         * @param y: Deuxieme Chiffre
+         * 
+         * @return: La Methode Retourne la somme de x et y
+         * 
+         */
         return x + y;
     }
 
 
-    public int subtract (int x, int y) { 
+    public int subtract (int x, int y) {
+        /*
+         * @brief Methode pour simuler une soustraction
+         * 
+         * @param x: Premiere Chiffre
+         * @param y: Deuxieme Chiffre
+         * 
+         * @return: La Methode Retourne la soustraction de x et y
+         * 
+         */
+
         return x - y; 
     }
    
