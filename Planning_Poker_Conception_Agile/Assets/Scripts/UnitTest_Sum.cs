25de48a517e4d0a09d6d96b7a17c148c35bab764
Seraphiel <76918503+Pierre2650@users.noreply.github.com>
2024-11-25 16:27:07 +0100

    White Space Correction and Another Doxygene Test

--- Planning_Poker_Conception_Agile/Assets/Scripts/UnitTest_Sum.cs
+++ Planning_Poker_Conception_Agile/Assets/Scripts/UnitTest_Sum.cs
@@ -8,21 +8,16 @@ using System.Collections.Generic;
 ///@author Larmat jean
 
 
-
 public class UnitTest_Sum
 {
-
     ///@class UnitTest_Sum_Class
     ///@brief Une Classe pour tester que ca fonctionne
 
     public int add(int x, int y)
     {
-
         /// @brief Methode pour simuler une addition
-
         /// @param x: Premiere Chiffre
         /// @param y: Deuxieme Chiffre
-
         /// @return: La Methode Retourne la somme de x et y
         
         return x + y;
@@ -33,10 +28,8 @@ public class UnitTest_Sum
     {
 
         ///@brief Methode pour simuler une soustraction
-
         ///@param x: Premiere Chiffre
         ///@param y: Deuxieme Chiffre
-
         ///@return: La Methode Retourne la soustraction de x et y
         
 
