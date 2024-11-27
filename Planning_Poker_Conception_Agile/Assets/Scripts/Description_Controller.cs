0e8e042137fab7397ac74bd6662f9c3c6c55dddb
Seraphiel <76918503+Pierre2650@users.noreply.github.com>
2024-11-27 11:33:52 +0100

    Stric mode Descrition
    -Controller  , Model and Visual works
    -Button  puts correct Description

--- Planning_Poker_Conception_Agile/Assets/Scripts/Description_Controller.cs
+++ Planning_Poker_Conception_Agile/Assets/Scripts/Description_Controller.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
 
 public class Description_Controller : MonoBehaviour
 {
     public GameObject descriptionField;
-    private TMP_InputField descriptionText;
+    private TMP_Text descriptionText;
     private void Start()
     {
-        descriptionText = descriptionField.GetComponent<TMP_InputField>();
+        descriptionText = descriptionField.GetComponent<TMP_Text>();
 
     }
     
@@ -21,7 +23,10 @@ public class Description_Controller : MonoBehaviour
         switch (index)
         {
             case 0:
-                descriptionText.text = "Hola como estas?";
+                descriptionText.text = "Mode de jeu :\r\n" +
+                    "Le mode de jeu Strict est le mode de base pour l'estimation de la difficulté des tâches. Les parties se déroulent avec un nombre de manches illimité, et les joueurs votent jusqu'à ce que l'unanimité soit atteinte.\r\n \r\n" +
+                    "À chaque tour, seuls les joueurs ayant attribué les valeurs les plus extrêmes(le plus haut et le plus bas) peuvent prendre la parole pour expliquer leur choix. Ces mêmes joueurs ont également accès à un Notepad dédié, où ils peuvent consigner des notes importantes pour aider à clarifier les décisions.\r\n \r\n" +
+                    "Ce mode encourage une analyse approfondie et une convergence progressive vers un consensus partagé par tous.";
                 break;
             case 1:
                 descriptionText.text = "Bien y tu?";
