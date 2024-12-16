95a2b30c3606e4e6cabb2700278a697226451c78
Pierre2650 <Pierre2650@users.noreply.github.com>
2024-12-16 22:42:02 +0000

    deploy: a3be3517bb01715d22b939d841b1f5787814e30f

--- html/search/classes_5.js
+++ html/search/classes_5.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['game_5fsettings_5fstart_5fbutton_5fcontroller_0',['Game_settings_Start_Button_Controller',['../classGame__settings__Start__Button__Controller.html',1,'']]],
-  ['go_5fbutton_5fcontroller_1',['Go_button_controller',['../classGo__button__controller.html',1,'']]]
+  ['game_5fcontroller_0',['Game_Controller',['../classGame__Controller.html',1,'']]],
+  ['game_5fsettings_5fstart_5fbutton_5fcontroller_1',['Game_settings_Start_Button_Controller',['../classGame__settings__Start__Button__Controller.html',1,'']]],
+  ['go_5fbutton_5fcontroller_2',['Go_button_controller',['../classGo__button__controller.html',1,'']]]
 ];
